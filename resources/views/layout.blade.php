0c97670e035e6ad12b2191f02e5fa6165a9a4093
1noseA <1noseA2L@gmail.com>
2020-07-09 21:22:44 +0900

    [Update]bootstrap/img-width

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>LaraCurry</title>
   @yield('styles')
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
 <header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ララカレー</a>
+  <nav class="navbar">
+    <a class="navbar-brand" href="/">ララカレー</a>
   </nav>
 </header>
 <main>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,18 @@
 @extends('layout')
 
 @section('content')
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col col-md-12">
-        <div class="top-image">
-          <img src="{{ asset('/img/top.jpg') }}">
-          <h3>インドカレーのテイクアウト</h3>
-          <h3>注文から20分で用意</h3>
-        </div>
+      <div class="top-image">
+        <img src="{{ asset('/img/top.jpg') }}">
+        <h3>インドカレーのテイクアウト</h3>
+        <h3>注文から20分で用意</h3>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col col-md-10 mx-auto">
         <div class="menu">
           <h3>メニュー</h3>
           <h4>カレー（辛さは4段階）</h4>
@@ -20,3 +24,4 @@
     </div>
   </div>
 @endsection
+
