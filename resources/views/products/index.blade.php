e3b8b351cb1dc9c4f7e13f09cfc6265319b32f72
1noseA <1noseA2L@gmail.com>
2020-07-09 19:44:59 +0900

    [Add]top-css

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,16 +4,17 @@
   <div class="container">
     <div class="row">
       <div class="col col-md-12">
-        <div class="takeout-image">
-          インドカレーのテイクアウト
-          注文から20分で用意
+        <div class="top-image">
+          <img src="{{ asset('/img/top.jpg') }}">
+          <h3>インドカレーのテイクアウト</h3>
+          <h3>注文から20分で用意</h3>
         </div>
         <div class="menu">
-          メニュー
-          選べるカレー（辛さは4段階）
-          ナンorライス
-          サイドメニュー
-          ドリンク
+          <h3>メニュー</h3>
+          <h4>カレー（辛さは4段階）</h4>
+          <h4>ナンorライス</h4>
+          <h4>サイドメニュー</h4>
+          <h4>ドリンク</h4>
         </div>
       </div>
     </div>
