ac71145f1d20beae3cbe1a3a505d8710919a8623
1noseA <1noseA2L@gmail.com>
2020-07-09 18:10:03 +0900

    [Add]product/index(home)

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>LaraCurry</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header>
+  <nav class="my-navbar">
+    <a class="my-navbar-brand" href="/">ララカレー</a>
+  </nav>
+</header>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-12">
+        <div class="takeout-image">
+          インドカレーのテイクアウト
+          注文から20分で用意
+        </div>
+        <div class="menu">
+          メニュー
+          選べるカレー（辛さは4段階）
+          ナンorライス
+          サイドメニュー
+          ドリンク
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+</body>
+<footer>
+</footer>
+</html>
