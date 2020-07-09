a6fdd153d8f42f7457e95f39f1c76a15cf16a931
1noseA <1noseA2L@gmail.com>
2020-07-09 23:50:08 +0900

    [Fix]moji-position

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,14 +5,13 @@
     <div class="row">
       <div class="top-image">
         <img src="{{ asset('/img/top.jpg') }}">
-        <h3>インドカレーのテイクアウト</h3>
-        <h3>注文から20分で用意</h3>
+        <h1>インドカレーのテイクアウト<br>ご来店までにご用意します</h1>
       </div>
     </div>
   </div>
   <div class="container-fluid">
     <div class="row">
-      <div class="col col-md-10 mx-auto">
+      <div class="col col-md-10 mx-auto my-5">
         <div class="menu">
           <h3>メニュー</h3>
           <h4>カレー（辛さは4段階）</h4>
