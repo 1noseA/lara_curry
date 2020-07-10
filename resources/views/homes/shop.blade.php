9a2f20b3b31b04a8753f61d1eb30e6ccc8c20781
1noseA <1noseA2L@gmail.com>
2020-07-10 10:16:42 +0900

    [Add]shop

---
+++ resources/views/homes/shop.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-6 mx-auto my-5">
+        <h2>店舗情報</h2>
+        {{-- 地図 --}}
+        <table class="table">
+          <tr>
+            <th>営業時間</th>
+            <td>11:00〜22:00</td>
+          </tr>
+            <th>定休日</th>
+            <td>なし</td>
+          </tr>
+            <th>アクセス</th>
+            <td>**線**駅徒歩5分</td>
+          </tr>
+            <th>住所</th>
+            <td>東京都杉並区********************</td>
+          </tr>
+            <th>電話番号</th>
+            <td>090-****-****</td>
+          </tr>
+            <th></th>
+            <td>看板メニューはララカレー<br>
+              チーズナンもおすすめ
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'ProductController@index');
+
+Route::get('/shop', 'HomeController@shop');
