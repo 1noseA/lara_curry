3a75c45b5d26348e43e3eb89d1a72793302f1906
1noseA <1noseA2L@gmail.com>
2020-07-12 09:21:14 +0900

    [Add]一覧に商品表示

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,7 +14,11 @@
       <div class="col-md-10 mx-auto my-5">
         <div class="menu">
           <h3>メニュー</h3>
-          <p><img src="../../uploads/{{ $product->image }}"></p>
+          @foreach ($products as $product)
+            {{-- <p><img src="../../uploads/{{ $product->image }}"></p> --}}
+            <p>{{ $product->name }}</p>
+            <p>{{ $product->price }}</p>
+          @endforeach
           <h4>カレー（辛さは4段階）</h4>
           <h4>ナンorライス</h4>
           <h4>サイドメニュー</h4>
