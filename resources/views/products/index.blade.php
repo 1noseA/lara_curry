9130ca60a68eeb4667a4f819c4a97783f4390917
1noseA <1noseA2L@gmail.com>
2020-07-12 09:28:03 +0900

    [要リファクタリング]カテゴリーごと商品表示

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,15 +14,41 @@
       <div class="col-md-10 mx-auto my-5">
         <div class="menu">
           <h3>メニュー</h3>
+          <h4>カレー（辛さは4段階）</h4>
           @foreach ($products as $product)
+            @if ($product->category === 1)
             {{-- <p><img src="../../uploads/{{ $product->image }}"></p> --}}
             <p>{{ $product->name }}</p>
             <p>{{ $product->price }}</p>
+            @endif
           @endforeach
-          <h4>カレー（辛さは4段階）</h4>
+
           <h4>ナンorライス</h4>
+          @foreach ($products as $product)
+            @if ($product->category === 2)
+            {{-- <p><img src="../../uploads/{{ $product->image }}"></p> --}}
+            <p>{{ $product->name }}</p>
+            <p>{{ $product->price }}</p>
+            @endif
+          @endforeach
+
           <h4>サイドメニュー</h4>
+          @foreach ($products as $product)
+            @if ($product->category === 3)
+            {{-- <p><img src="../../uploads/{{ $product->image }}"></p> --}}
+            <p>{{ $product->name }}</p>
+            <p>{{ $product->price }}</p>
+            @endif
+          @endforeach
+
           <h4>ドリンク</h4>
+          @foreach ($products as $product)
+            @if ($product->category === 4)
+            {{-- <p><img src="../../uploads/{{ $product->image }}"></p> --}}
+            <p>{{ $product->name }}</p>
+            <p>{{ $product->price }}</p>
+            @endif
+          @endforeach
         </div>
       </div>
     </div>
