7318c1e52c7c429c5cb42957a3c692ec6b9e6cd5
1noseA <1noseA2L@gmail.com>
2020-07-17 12:23:44 +0900

    [?]やっぱりできない(quantityがnull）

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,25 +16,25 @@ class CartController extends Controller
 
     public function add(Request $request)
     {
-        $cart = new Cart;
-        $cart->user_id = Auth::user();
-        // $cart->product_id = $request->product_id;
-        if($request->product_id == $product_id){
-            $cart->quantity += $request->quantity;
+        // $cart = new Cart;
+        $user_id = Auth::id();
+        $product_id = $request->product_id;
+        // $quantity = $request->quantity;
+        $cart_info=Cart::firstOrCreate(['product_id' => $product_id, 'user_id' => $user_id, 'quantity' => $request->quantity]);
+        if($cart_info->wasRecentlyCreated){
+            // $cart->save();
+            $message = '注文リストに追加しました';
+        } else {
+            $quantity += $request->quantity;
             $cart->update();
             $message = '個数を追加しました';
-        } else {
-            $cart->quantity = $request->quantity;
-            $cart->save();
-            $message = '注文リストに追加しました';
         }
         $carts = Cart::where('user_id', Auth::user()->id)->where('product_id', $request['product_id'])->get();
-        return view('carts.index',compact('carts' , 'message'));
+        return redirect('carts.index',compact('carts' , 'message'));
     }
 
     public function destroy(Cart $cart)
     {
-        // $cart = Cart::find($id);
         $cart->delete();
         return redirect('/cart');
     }
