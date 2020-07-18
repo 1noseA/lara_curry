4374fd26a6815bddae81d60317d657ba5c066727
1noseA <1noseA2L@gmail.com>
2020-07-18 19:08:48 +0900

    [Add]orderコントローラーとルーティング追加

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    // お客様情報入力画面表示
+    public function create()
+    {
+
+    }
+
+    // お客様情報送信
+    public function store(Request $request)
+    {
+
+    }
+
+    // 注文情報一覧
+    public function index()
+    {
+
+    }
+
+    // 注文商品の表示
+    public function show()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('product', 'ProductController');
 
 Route::resource('cart', 'CartController')->middleware('auth');
+
+Route::resource('order', 'OrderController')->middleware('auth');
