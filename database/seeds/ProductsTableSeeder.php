9997b5677caf032f123ab3423802ea152bbe8ce7
1noseA <1noseA2L@gmail.com>
2020-07-22 23:17:03 +0900

    [Update]seeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -17,36 +17,48 @@ class ProductsTableSeeder extends Seeder
                 'price' => 400,
                 'text' => '看板メニュー。豆カレー',
                 'category' => 1,
+                'hot' => 2,
+                'image' => 'curry_pot.png',
             ],
             [
                 'name' => 'バターチキン',
                 'price' => 400,
                 'text' => '一番人気',
                 'category' => 1,
+                'hot' => 2,
+                'image' => 'matsaman.png',
             ],
             [
                 'name' => 'キーマ',
                 'price' => 400,
                 'text' => 'ひき肉ごろごろ',
                 'category' => 1,
+                'hot' => 3,
+                'image' => 'keema_curry.png',
             ],
             [
                 'name' => 'マトン',
                 'price' => 400,
                 'text' => 'スパイシーで臭みは少ない',
                 'category' => 1,
+                'hot' => 4,
+                'image' => 'red_curry.png',
             ],
             [
                 'name' => 'エビ',
                 'price' => 400,
                 'text' => 'エビがぷりぷり',
                 'category' => 1,
+                'hot' => 3,
+                'image' => 'yello_curry.png',
             ],
             [
                 'name' => 'ほうれん草',
                 'price' => 400,
                 'text' => 'ヘルシー',
                 'category' => 1,
+                'hot' => 2,
+                'image' => 'green_curry.png',
             ],
 
             [
@@ -54,18 +66,21 @@ class ProductsTableSeeder extends Seeder
                 'price' => 200,
                 'text' => 'でかさは負けない',
                 'category' => 2,
+                'image' => 'nan.png',
             ],
             [
                 'name' => 'チーズナン',
                 'price' => 300,
                 'text' => 'チーズのびのび',
                 'category' => 2,
+                'image' => 'pan.png',
             ],
             [
                 'name' => 'ライス',
                 'price' => 200,
                 'text' => '200g',
                 'category' => 2,
+                'image' => 'rice.png',
             ],
 
             [
@@ -73,18 +88,21 @@ class ProductsTableSeeder extends Seeder
                 'price' => 200,
                 'text' => 'コールスロー',
                 'category' => 3,
+                'image' => 'salad.png',
             ],
             [
                 'name' => 'タンドリーチキン',
                 'price' => 300,
                 'text' => 'ジュージー',
                 'category' => 3,
+                'image' => 'chicken.png',
             ],
             [
                 'name' => 'サモサ',
                 'price' => 300,
                 'text' => '揚げ餃子みたいな。じゃがいも＋ひき肉＋豆',
                 'category' => 3,
+                'image' => 'samosa.png',
             ],
 
             [
@@ -92,18 +110,21 @@ class ProductsTableSeeder extends Seeder
                 'price' => 200,
                 'text' => 'ヨーグルトドリンク',
                 'category' => 4,
+                'image' => 'drink_lassi.png',
             ],
             [
                 'name' => 'マンゴーラッシー',
                 'price' => 300,
                 'text' => 'とろーり濃厚',
                 'category' => 4,
+                'image' => 'lassi_mango.png',
             ],
             [
                 'name' => 'チャイ',
                 'price' => 300,
                 'text' => 'スパイシーなミルクティー',
                 'category' => 4,
+                'image' => 'chai.png',
             ],
         ]);
     }
