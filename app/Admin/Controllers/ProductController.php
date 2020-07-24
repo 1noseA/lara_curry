5866c7470494f30d00fe418f88cc5029b114d5ef
1noseA <1noseA2L@gmail.com>
2020-07-24 12:34:32 +0900

    [Add]管理画面にproductsを追加

---
+++ app/Admin/Controllers/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Product;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ProductController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Product';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('price', __('Price'));
+        $grid->column('text', __('Text'));
+        $grid->column('hot', __('Hot'));
+        $grid->column('category', __('Category'));
+        $grid->column('image', __('Image'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('price', __('Price'));
+        $show->field('text', __('Text'));
+        $show->field('hot', __('Hot'));
+        $show->field('category', __('Category'));
+        $show->field('image', __('Image'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product());
+
+        $form->text('name', __('Name'));
+        $form->number('price', __('Price'));
+        $form->text('text', __('Text'));
+        $form->number('hot', __('Hot'))->default(1);
+        $form->number('category', __('Category'));
+        $form->image('image', __('Image'));
+
+        return $form;
+    }
+}
